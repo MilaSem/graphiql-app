664ab54d7e33a012c23a77fa463595a0286516a6
Smetan-dot <smetanaandrei@gmail.com>
2023-12-22 21:51:06 +0300

    fix: change wrong behavior when user is log-in on the welcome page

--- src/pages/welcomePage/welcomePage.tsx
+++ src/pages/welcomePage/welcomePage.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { type FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase';
+import { type FC, useEffect } from 'react';
 
 export const WelcomePage: FC = () => {
+  const navigate = useNavigate();
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate('/qraphql');
+  }, [user, loading]);
   return <>Welcome page works!</>;
 };
