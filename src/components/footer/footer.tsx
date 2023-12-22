8e214dc873f75b4a45f2852b01b3af22f9c6b475
Smetan-dot <smetanaandrei@gmail.com>
2023-12-22 21:34:26 +0300

    feat: add footer with nesessary links and course-logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,11 +2,48 @@ import { type FC } from 'react';
 
 import './footer.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Footer: FC = () => {
   return (
     <>
-      <footer className="footer"></footer>
+      <footer className="footer">
+        <p className="footer-text">
+          2024 Created by:
+          <Link
+            className="footer-link"
+            to={'https://github.com/AnnaFeona'}
+            target="_blank"
+          >
+            {' '}
+            AnnaFeona
+          </Link>
+          ,
+          <Link
+            className="footer-link"
+            to={'https://github.com/Smetan-dot'}
+            target="_blank"
+          >
+            {' '}
+            Smetan-dot
+          </Link>{' '}
+          and
+          <Link
+            className="footer-link"
+            to={'https://github.com/MilaSem'}
+            target="_blank"
+          >
+            {' '}
+            MilaSem
+          </Link>
+        </p>
+        <Link to={'https://rs.school/react/'} target="_blank">
+          <img
+            className="footer-image"
+            src="https://rs.school/images/rs_school_js.svg"
+          ></img>
+        </Link>
+      </footer>
     </>
   );
 };
