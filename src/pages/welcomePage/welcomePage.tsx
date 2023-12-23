10ae19adef640e6637f804eff3551358116880d1
Smetan-dot <smetanaandrei@gmail.com>
2023-12-24 00:43:54 +0300

    fix: remove redirect from welcome-page when user excist and add sticky header

--- src/pages/welcomePage/welcomePage.tsx
+++ src/pages/welcomePage/welcomePage.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../../firebase';
-import { type FC, useEffect } from 'react';
+import { type FC } from 'react';
 
 export const WelcomePage: FC = () => {
-  const navigate = useNavigate();
-  const [user, loading] = useAuthState(auth);
-
-  useEffect(() => {
-    if (loading) return;
-    if (user) navigate('/qraphql');
-  }, [user, loading]);
   return <>Welcome page works!</>;
 };
